7d2946ea51bb7ac3556f693fe9e763ba1fad6e2d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-09-05 11:30:40 +0200

    remove wintest.py - integrated into window.py

--- apssh/window.py
+++ apssh/window.py
@@ -42,3 +42,41 @@ async def gather_window(*futures, window=1, loop=None, debug=False, **kwds):
         wrapped_futures.append(monitor_queue())
     overall = await asyncio.gather(*wrapped_futures, loop=loop, **kwds)
     return overall
+
+############################## a basic test for gather_window
+def wintest():
+    """
+    a simple tool for testing gather_window
+    """
+
+    async def tick(id, n):
+        for i in range(n):
+            print("id={} - tick {}".format(id, i))
+            await asyncio.sleep(0.5)
+        return id
+
+    parser = ArgumentParser()
+    parser.add_argument("-w", "--window", default=None, type=int,
+                        help="default is to use regular asyncio.gather")
+    parser.add_argument("-f", "--from", dest='from_arg', default=1, type=int)
+    parser.add_argument("-t", "--to", default=3, type=int)
+    args = parser.parse_args()
+
+    assert args.from_arg < args.to, "need to define from < to"
+
+    r = range(args.from_arg, args.to) 
+    issued = [ print("Creating tick({})".format(x)) for x in r ]
+
+    ticks = [ tick(i, i) for i in r ]
+
+    if args.window:
+        print("Using window={}".format(args.window))
+        results = asyncio.get_event_loop().run_until_complete(gather_window(*ticks, window=args.window))
+    else:
+        print("Using regular gather")
+        results = asyncio.get_event_loop().run_until_complete(asyncio.gather(*ticks))
+
+        print(results)
+
+if __name__ == '__main__':
+    test_gather_window()

0a00aeed6ed0f2fe4906cc58bf25137ab24e5262
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-09-14 12:59:25 +0200

    faster test shells using perl to sleep

--- asynciojobs/sshtests2.sh
+++ asynciojobs/sshtests2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 message=$1
-delay=$(( $RANDOM / 8000)); echo $message: waiting for $delay on $(hostname) ; sleep $delay; echo $message done on $(hostname)
+delay=$(( $RANDOM / 8000)); echo $message: waiting for 0.${delay}s on $(hostname) ; perl -e "select(undef,undef,undef,0.${delay});"; echo $message done on $(hostname)

431f14ef12bb8e28f4b710e94c247966f95f7d1b
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-09-15 10:35:24 +0200

    This closes #1

--- apssh/config.py
+++ apssh/config.py
@@ -1,8 +1,9 @@
 import os
+import pwd
 import time
 
 default_time_name =             time.strftime("%Y-%m-%d@%H:%M")
-default_username =              os.getlogin()
+default_username =              pwd.getpwuid(os.getuid())[0]
 default_timeout =               30
 default_private_key =           os.path.expanduser("~/.ssh/id_rsa")
 # dont use expanduser as this is relative to a remote system

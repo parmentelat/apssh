6313573f76bebad3467c332829105a7ce12406e0
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-10-31 14:05:21 +0100

    expose more symbols at the package level

--- apssh/__init__.py
+++ apssh/__init__.py
@@ -1,8 +1,17 @@
 __all__ = []
 
+# basic tools to deal with keys
 from .keys import load_agent_keys, import_private_key
 __all__ += [ load_agent_keys, import_private_key ]
 
-from .jobs.sshjobs import SshNode, SshJob, SshJobScript, SshJobCollector, SshJobPusher
+# basic ssh connections and sessions
+from .sshproxy import SshProxy
+__all__ += [ SshProxy ]
 
+# how to format outputs
+from .formatters import RawFormatter, ColonFormatter, TimeColonFormatter
+__all__ += [ RawFormatter, ColonFormatter, TimeColonFormatter ]
+
+# jobs for asynciojobs
+from .jobs.sshjobs import SshNode, SshJob, SshJobScript, SshJobCollector, SshJobPusher
 __all__ +=  [ SshNode, SshJob, SshJobScript, SshJobCollector, SshJobPusher ]

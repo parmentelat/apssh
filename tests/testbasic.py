9e9774b604f4ff9a40c846bd189cba06248ec8e7
parmentelat <thierry.parmentelat@inria.fr>
2018-03-28 10:55:21 +0200

    extend tests vor visual checks of graphical outputs in miscell configs

--- tests/testbasic.py
+++ tests/testbasic.py
@@ -26,13 +26,63 @@ class TestBasic(unittest.TestCase):
             SshJob(
                 node=gateway,
                 commands=[
-                    RunString("#!/usr/bin/env bash\necho with RunsString on $(hostname) at $(date)"),
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo with RunsString on $(hostname) at $(date)"),
                 ]),
             SshJob(
                 node=gateway,
                 commands=[
                     RunScript("tests/testbasic.sh"),
                 ]),
+            SshJob(
+                node=gateway,
+                commands=[
+                    Run('wc -l /etc/passwd'),
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo with RunsString on $(hostname) at $(date)",
+                              remote_name="show-host-date"),
+                    RunScript("tests/testbasic.sh"),
+                ]),
+            SshJob(
+                node=gateway,
+                commands=[
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo first arg is $1\n",
+                              10
+                    )
+            ]),
+            SshJob(
+                node=gateway,
+                commands=[
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo first arg is $1\n",
+                              10,
+                              remote_name = 'short-show-args'
+                    )
+            ]),
+            SshJob(
+                node=gateway,
+                commands=[
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo first arg is $1\n"
+                              "echo second arg is $2\n"
+                              "echo third arg is $3\n"
+                              "echo fourth arg is $4\n",
+                              100, 200, 300, 400
+                    )
+            ]),
+            SshJob(
+                node=gateway,
+                commands=[
+                    RunString("#!/usr/bin/env bash\n"
+                              "echo first arg is $1\n"
+                              "echo second arg is $2\n"
+                              "echo third arg is $3\n"
+                              "echo fourth arg is $4\n",
+                              1000, 2000, 3000, 4000,
+                              remote_name = 'long-show-args'
+                    )
+            ]),
             scheduler=scheduler,
         )
 

6581ce630a6794f8de5ca02384577a9430c5de77
parmentelat <thierry.parmentelat@inria.fr>
2018-04-17 15:43:31 +0200

    fix locate_file for binary apssh

--- apssh/config.py
+++ apssh/config.py
@@ -14,10 +14,10 @@ default_time_name = time.strftime("%Y-%m-%d@%H:%M")
 default_username = pwd.getpwuid(os.getuid())[0]
 default_timeout = 30
 default_private_keys = (
-    Path("~/.ssh/id_rsa").expanduser(),
-    Path("~/.ssh/id_dsa").expanduser(),
+    Path.home() / ".ssh/id_rsa",
+    Path.home() / ".ssh/id_dsa",
 )
 
-local_config_dir = Path.home() / "~/.apssh"
+local_config_dir = Path.home() / ".apssh"
 # dont use expanduser as this is relative to a remote system
 default_remote_workdir = ".apssh-remote"

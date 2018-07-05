0390b54e91a5882fc9f8918196264a1b95154938
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-07-05 15:23:00 +0200

    first test on counting connections + description of the new test setup

---
+++ tests/tests_connections.py
@@ -0,0 +1,96 @@
+# pylint: disable=c0111
+
+import time
+import asyncio
+
+from unittest import TestCase
+
+from .util import in_out_connections
+
+from asynciojobs import Scheduler, Job
+from apssh import SshNode, SshJob, ColonFormatter
+
+def localuser():
+    import os
+    return os.environ['LOGNAME']
+
+class Tests(TestCase):
+
+    def simple(self, hostname='localhost', username=None,
+               *, share_connection=False, number=5):
+        """
+        one node 1 hop away
+        create one or <number> connections (depending on share_connection)
+        and run <number> times 'hostname'
+        check the number of alive connections
+        """
+        if username is None:
+            username = localuser()
+
+        connections = number if not share_connection else 1
+
+        print(f"creating {number} commands on {connections} connections"
+              f" to {username}@{hostname}")
+        scheduler = Scheduler()
+        nodes = []
+        jobs = []
+        for i in range(number):
+            if share_connection:
+                if i == 0:
+                    node = SshNode(hostname, username=username,
+                                   formatter=ColonFormatter(verbose=False))
+                    nodes.append(node)
+                else:
+                    pass
+            else:
+                node = SshNode(hostname, username=username,
+                               formatter=ColonFormatter(verbose=False))
+                nodes.append(node)
+            jobs.append(SshJob(node=node,
+                               command='hostname',
+                               scheduler=scheduler))
+
+        # record base status
+        in0, out0 = in_out_connections()
+        print(f"INITIAL count in={in0} out={out0}")
+
+        scheduler.run()
+
+        in1, out1 = in_out_connections()
+        print(f"AFTER RUN in={in1} out={out1}")
+        self.assertEqual(in1-in0, connections)
+        self.assertEqual(out1-out0, connections)
+
+        # cleanup
+        gathered = asyncio.get_event_loop().run_until_complete(
+            asyncio.gather(*(node.close() for node in nodes)))
+        in1, out1 = in_out_connections()
+        print(f"AFTER CLEANUP in={in1} out={out1}")
+        self.assertEqual(in1-in0, 0)
+        self.assertEqual(out1-out0, 0)
+
+
+    def test_simple_1(self):
+        self.simple(share_connection=True)
+
+    def test_simple_n(self):
+        self.simple(share_connection=False)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-H", "--hostname", default=None)
+    parser.add_argument("-U", "--username", default=None)
+    parser.add_argument("-n", "--number", default=10, type=int)
+    parser.add_argument("-t", "--timeout", default=1., type=float)
+    parser.add_argument("-s", "--share-connection", action='store_false',
+                        default=True)
+    args = parser.parse_args()
+
+    Tests().simple(hostname=args.hostname,
+                   username=args.username,
+                   number=args.number,
+                   share_connection=args.share_connection)
+
+if __name__ == '__main__':
+    main()
--- tests/util.py
+++ tests/util.py
@@ -1,10 +1,8 @@
 # pylint: disable=c0111
 
-import asyncio
-
+import subprocess
+import platform
 from pathlib import Path
-
-from asynciojobs import Watch, Job, Scheduler
 
 def produce_png(scheduler, name):
     dot = scheduler.graph()
@@ -17,3 +15,52 @@ def produce_png(scheduler, name):
         actual_name = name
     dot.render(actual_name)
     print(f"png file produced in {actual_name}{{,.png}}")
+
+
+def count_ssh_connections(incoming:bool, outgoing:bool):
+
+    """
+    tool for counting open connections
+
+    based on linux's 'ss' command for now
+    """
+
+
+
+    if platform.system() != "Linux":
+        raise ValueError(f"inspector works only on linux boxes")
+    if not incoming and not outgoing:
+        return 0
+    filters = []
+    if incoming:
+        filters.append("sport = :ssh")
+    if outgoing:
+        filters.append("dport = :ssh")
+    command = []
+    command.append("ss")
+    command += "-o state established".split()
+    filter = "( " + " or ".join(filters) + " )"
+    command.append(filter)
+#    print(" ".join(command))
+    completed = subprocess.run(command, stdout=subprocess.PIPE)
+    count = 0
+#    lines = completed.stdout.decode(encoding='utf8').split('\n')
+    for line in completed.stdout.split(b"\n"):
+        if not line or b"Netid" in line:
+            continue
+#        print(f"counting line {line}")
+        count += 1
+    return count
+
+def incoming_connections():
+    return count_ssh_connections(incoming=True, outgoing=False)
+
+def outgoing_connections():
+    return count_ssh_connections(outgoing=True, incoming=False)
+
+def in_out_connections():
+    return incoming_connections(), outgoing_connections()
+
+if __name__ == '__main__':
+    print(f"incoming {incoming_connections()}")
+    print(f"outgoing {outgoing_connections()}")

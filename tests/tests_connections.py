91ff482b77228c61a2f2a7aea1e9d185449a776c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2018-07-06 10:18:51 +0200

    tests_connections now also has 2-hops scenarios

--- tests/tests_connections.py
+++ tests/tests_connections.py
@@ -12,39 +12,34 @@ from .util import localuser, in_out_connections
 
 class Tests(TestCase):
 
-    def simple(self, hostname='localhost', username=None,
-               *, share_connection=False, number=5):
+    def hop1(self, hostname='localhost', username=None,
+             *, c1, commands):
         """
-        one node 1 hop away
-        create one or <number> connections (depending on share_connection)
-        and run <number> times 'echo simple{i}'
-        check the number of alive connections
+        create
+          * <c1> connections to one node 1 hop away
+          * and on each <commands> commands
+
+        check current number of connections
         """
         if username is None:
             username = localuser()
 
-        connections = number if not share_connection else 1
-
-        print(f"creating {number} commands on {connections} connections"
+        print(f"creating {c1} hop1-connections - "
+              f"{commands} commands per conn - "
               f" to {username}@{hostname}")
         scheduler = Scheduler()
         nodes = []
         jobs = []
-        for i in range(number):
-            if share_connection:
-                if i == 0:
-                    node = SshNode(hostname, username=username,
-                                   formatter=ColonFormatter(verbose=False))
-                    nodes.append(node)
-                else:
-                    pass
-            else:
-                node = SshNode(hostname, username=username,
-                               formatter=ColonFormatter(verbose=False))
-                nodes.append(node)
-            jobs.append(SshJob(node=node,
-                               command=f'echo simple{i}',
-                               scheduler=scheduler))
+        for n in range(c1):
+            node1 = SshNode(hostname, username=username,
+                            formatter=ColonFormatter(verbose=False))
+            nodes.append(node1)
+            for c in range(commands):
+                jobs.append(SshJob(node=node1,
+                                   command=f'echo hop1-{n}-{c}',
+                                   scheduler=scheduler))
+
+        expected = c1
 
         # record base status
         in0, out0 = in_out_connections()
@@ -54,8 +49,8 @@ class Tests(TestCase):
 
         in1, out1 = in_out_connections()
         print(f"AFTER RUN in={in1} out={out1}")
-        self.assertEqual(in1-in0, connections)
-        self.assertEqual(out1-out0, connections)
+        self.assertEqual(in1-in0, expected)
+        self.assertEqual(out1-out0, expected)
 
         # cleanup
         gathered = asyncio.get_event_loop().run_until_complete(
@@ -65,22 +60,84 @@ class Tests(TestCase):
         self.assertEqual(in1-in0, 0)
         self.assertEqual(out1-out0, 0)
 
-    def test_simple_1(self):
-        self.simple(share_connection=True)
-
-    def test_simple_n(self):
-        self.simple(share_connection=False)
-
     def hop2(self, hostname='localhost', username=None,
-             *, share_connection=False, number=5):
+             *, c1=1, c2=1, commands=1):
         """
-        same but with 2 hops
-        create either one or <number> level1 connections (depending on share_connection)
-        then <number> times, create a 2-hop connection (ditto)
-        and on each of these, run 'hostname'
-        check the number of alive connections
+        create
+          * <c1> connections to one node 1 hop away
+          * on each one, <c2> connections one hop behind
+          * and on each <commands> commands
+
+        check current number of connections
         """
-        pass
+        if username is None:
+            username = localuser()
+
+        print(f"creating {c1}x{c2} hop2-connections - "
+              f"{commands} commands per conn "
+              f" to {username}@{hostname}")
+        scheduler = Scheduler()
+        nodes = []
+        jobs = []
+        for n in range(c1):
+            node1 = SshNode(hostname, username=username,
+                            formatter=ColonFormatter(verbose=False))
+            nodes.append(node1)
+            for m in range(c2):
+                node2 = SshNode(hostname, username=username,
+                                gateway=node1,
+                                formatter=ColonFormatter(verbose=False))
+                for c in range(commands):
+                    jobs.append(SshJob(node=node2,
+                                       command=f'echo hop1-{n}-{m}-{c}',
+                                       scheduler=scheduler))
+
+        # for each hop1 conn, there are 1 hop1 + c2 hop2 connections alive
+        expected = c1 * (c2+1)
+
+        # record base status
+        in0, out0 = in_out_connections()
+        print(f"INITIAL count in={in0} out={out0}")
+
+        scheduler.run()
+
+        in1, out1 = in_out_connections()
+        print(f"AFTER RUN in={in1} out={out1}")
+        self.assertEqual(in1-in0, expected)
+        self.assertEqual(out1-out0, expected)
+
+        # cleanup
+        # pretend there is no order in there
+        nodes = set(nodes)
+        gathered = asyncio.get_event_loop().run_until_complete(
+            asyncio.gather(*(node.close() for node in nodes)))
+        in1, out1 = in_out_connections()
+        print(f"AFTER CLEANUP in={in1} out={out1}")
+        self.assertEqual(in1-in0, 0)
+        self.assertEqual(out1-out0, 0)
+
+
+    def test_hop1_shared(self):
+        self.hop1(c1=1, commands=4)
+
+    def test_hop1_dup(self):
+        self.hop1(c1=4, commands=1)
+
+    def test_hop1_multi(self):
+        self.hop1(c1=4, commands=4)
+
+
+    def test_hop2_112(self):
+        self.hop2(commands=2)
+
+    def test_hop2_121(self):
+        self.hop2(c2=2)
+
+    def test_hop2_211(self):
+        self.hop2(c1=2)
+
+    def test_hop2_222(self):
+        self.hop2(c1=2, c2=2, commands=2)
 
 
 def main():

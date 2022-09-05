472314f24a2592c966d57ecf965987b6d3e8a3ab
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-09-05 17:38:53 +0200

    fix doc build on readthedocs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinxcontrib.asyncio',
     'sphinxcontrib.napoleon',
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
